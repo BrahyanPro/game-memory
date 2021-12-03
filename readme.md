ce851a07bb6dc38a8141d4d43fe8c784c69d3a9d
BrahyanPro <brahyanpro16@gmail.com>
2021-12-03 11:16:49 -0400

    first commi

---
+++ readme.md
@@ -0,0 +1,29 @@
+# Memory Game
+
+Un juego de memoria, donde los usuarios deben recordar las posiciones de las tarjetas y abrir dos imágenes exactas secuencialmente para ganar puntos. Una vez que se abren todas las tarjetas, el usuario sale victorioso.
+
+## Skill
+
+- React
+- [Vite](https://vitejs.dev/) `Hechenle un ojo a vite`
+- CSS3
+- Yarn
+
+## Features
+
+1. Traking de Movimientos
+2. Reiniciar juego
+
+## Installation
+
+1. `git clone https://github.com/BrahyanPro/memory-game.git`
+
+2. `cd memory-game`
+
+3. `yarn`
+
+4. `yarn dev`
+
+## License
+
+The project is under [MIT license](https://choosealicense.com/licenses/mit/).
---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  { src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false },
+  { src: '/img/ring-1.png', matched: false },
+  { src: '/img/scroll-1.png', matched: false },
+  { src: '/img/shield-1.png', matched: false },
+  { src: '/img/sword-1.png', matched: false },
+];
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000); // wait a second before reset the turn
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // start a new game automatically
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  // console.log(cards);
+
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+    console.log(card);
+  };
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1); // this adds a callback function to setTurns which is previous state
+    setDisabled(false);
+  };
+
+  return (
+    <>
+      <div className="App">
+        <h1>Magic Match</h1>
+        <h2 className="creador">
+          By Brahyan Pro&copy; - Dominican &#127465;&#127476;
+        </h2>
+        <button onClick={shuffleCards}>New Game</button>
+
+        <div className="card-grid">
+          {cards.map((card) => (
+            <SingleCard
+              key={card.id}
+              card={card}
+              handleChoice={handleChoice}
+              flipped={card === choiceOne || card === choiceTwo || card.matched}
+              disabled={disabled}
+            />
+          ))}
+        </div>
+        <p>Turns: {turns}</p>
+      </div>
+      <p className="abajo">BAMM &#127465;&#127476;</p>
+    </>
+  );
+}
+
+export default App;
